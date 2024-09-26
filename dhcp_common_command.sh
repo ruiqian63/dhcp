b35b23c4390a3adad5c9dd24f8aa036e5c854783
ruiqian63 <ruiqian0603@gmail.com>
2024-09-26 15:45:48 -0400

    Update dhcp_common_command.sh

--- dhcp_common_command.sh
+++ dhcp_common_command.sh
@@ -1,2 +1,12 @@
-#neonatal data path
+ssh -XY rui.qian@navarro
+
+#neonatal raw data path
 cd /neuro/labs/grantlab/users/rui.qian/Package_1232682/fmriresults01/rel3_derivatives
+#subject list
+/neuro/labs/grantlab/users/rui.qian/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline/subject_neonatal_func.txt
+
+
+#genome(plink) raw data path
+cd /neuro/labs/grantlab/users/rui.qian/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files
+#subject list
+/neuro/labs/grantlab/users/rui.qian/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files/subjects.txt
