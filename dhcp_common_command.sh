444d204cbfc50edaafc5ff340576901aed10e78a
ruiqian63 <ruiqian0603@gmail.com>
2024-09-27 14:24:02 -0400

    Update dhcp_common_command.sh

--- dhcp_common_command.sh
+++ dhcp_common_command.sh
@@ -10,3 +10,7 @@ cd /neuro/labs/grantlab/users/rui.qian/Package_1232682/fmriresults01/rel3_deriva
 cd /neuro/labs/grantlab/users/rui.qian/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files
 #subject list
 /neuro/labs/grantlab/users/rui.qian/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files/subjects.txt
+
+#add toolbox to matlab
+scp /Users/Rui/downloads/spm12 rui.qian@navarro:/neuro/arch/x86_64-Linux/packages/matlab/R2018a/toolbox
+
