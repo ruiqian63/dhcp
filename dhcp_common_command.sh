b92dd332c0109ea06e3d751e5f5ba490cc67f0fe
ruiqian63 <ruiqian0603@gmail.com>
2024-10-03 11:40:21 -0400

    Update dhcp_common_command.sh

--- dhcp_common_command.sh
+++ dhcp_common_command.sh
@@ -2,15 +2,15 @@ ssh -XY rui.qian@navarro
 matlab -v 2018a
 
 #neonatal raw data path
-cd /neuro/labs/grantlab/users/rui.qian/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline
+cd /neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline
 #subject list
-/neuro/labs/grantlab/users/rui.qian/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline/subject_neonatal_func.txt
+/neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline/subject_neonatal_func.txt
 
 
 #genome(plink) raw data path
-cd /neuro/labs/grantlab/users/rui.qian/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files
+cd /neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files
 #subject list
-/neuro/labs/grantlab/users/rui.qian/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files/subjects.txt
+/neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/Package_1232682/genomics_sample03/Users/nickharper/cdb/dhcp_plink_files/subjects.txt
 
 #add toolbox to matlab
 scp -r /Users/Rui/downloads/spm12 rui.qian@navarro:/neuro/arch/x86_64-Linux/packages/matlab/R2018a/toolbox
