aff3398f8cad793d0e22f4921f323223a498f356
ruiqian63 <ruiqian0603@gmail.com>
2024-10-04 18:08:34 -0400

    Update dhcp_common_command.sh

--- dhcp_common_command.sh
+++ dhcp_common_command.sh
@@ -18,3 +18,5 @@ scp -r /Users/Rui/downloads/spm12 rui.qian@navarro:/neuro/labs/grantlab/research
 
 brith age (week)
 Min: 23.0 Max: 42.71428571
+
+scp ch258782@e3-login.tch.harvard.edu:/neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline/sub-CC00056XX07/ses-10700/func/sub-CC00056XX07_ses-10700_task-rest_desc-preproc_bold.nii.gz  /Users/Rui/documents/dhcp
