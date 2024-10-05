ef2a4d56bd4d0ec9e7b12f99e88c550fa730400f
ruiqian63 <ruiqian0603@gmail.com>
2024-10-04 21:56:29 -0400

    Update dhcp_common_command.sh

--- dhcp_common_command.sh
+++ dhcp_common_command.sh
@@ -21,4 +21,4 @@ brith age (week)
 Min: 23.0 Max: 42.71428571
 
 scp ch258782@e3-login.tch.harvard.edu:/neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline/sub-CC00056XX07/ses-10700/func/sub-CC00056XX07_ses-10700_task-rest_desc-preproc_bold.nii.gz  /Users/Rui/documents/dhcp
-scp /Users/Rui/documents/dhcp/Reslice2_template_t2.nii ch258782@e3-login.tch.harvard.edu:/neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/Package_1232682/fmriresults01/rel3_derivatives/rel3_dhcp_fmri_pipeline/sub-CC00056XX07/ses-10700/func
+scp /Users/Rui/documents/dhcp/Reslice2_template_t2.nii ch258782@e3-login.tch.harvard.edu:/neuro/labs/grantlab/research/enrique.mondragon/morton_lab/dhcp/data/registration/template
